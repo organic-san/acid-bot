07af38bc27a3c45526be5793556b7d2ef39ecc7b
organic_san <yuki.0710.happy@gmail.com>
2021-09-29 19:02:30 +0800

    add: interaction/words

--- commands/poll.js
+++ commands/poll.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js');
-const textCommand = require('../JSmodule/textModule');
 
 module.exports = {
 	data: new SlashCommandBuilder()
